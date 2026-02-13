09969f2c5fd1f44ffbb57ba19bed02b2b71ba3bb
axel1 <20099078@mail.wit.ie>
2026-02-13 09:39:37 +0000

    week4

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
 
+import { create } from 'express-handlebars';
 import logger from "./utils/logger.js";
 import express from 'express';
 import routes from './routes.js'; 
@@ -7,6 +8,9 @@ import routes from './routes.js';
 const app = express();
 const port = 3000;
 
+const handlebars = create({extname: '.hbs'});
+app.engine(".hbs", handlebars.engine);
+app.set("view engine", ".hbs");
 app.use("/", routes);
 
 app.listen(port, () => logger.info(`Your app is listening on port ${port}!`));
